cdc698df1cf7758b93393d23a6bc8f54df509bad
zzx <1059311525@qq.com>
2023-05-02 01:06:17 +0800

    agg 19

---
+++ AGG/Vue2021/mine/vue_basic/19_单文件组件/School.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>校名：{{ name }}</h2>
+    <student></student>
+  </div>
+</template>
+
+<script>
+import Student from './Student.vue';
+
+export default {
+  name: 'School',
+  components: {
+    Student,
+  },
+  data() {
+    return {
+      name: '第一中学',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ AGG/Vue2021/mine/vue_basic/19_单文件组件/Student.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>姓名：{{ name }}</h3>
+    <h3>年龄：{{ age }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: 'Jaeger',
+      age: 15,
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
