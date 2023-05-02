17d6866ed7a6850c890efc1884ee6cde3469e88a
zzx <1059311525@qq.com>
2023-05-02 22:45:13 +0800

    agg vue_test src_03_props传值

---
+++ AGG/Vue2021/mine/02_vue_test/src_02_ref属性/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <school></school>
+    <hr>
+    <student></student>
+    <student></student>
+    <button ref="btn" @click="show">打印组件</button>
+    <h4 ref="btn">======</h4>
+  </div>
+</template>
+
+<script>
+import School from '@/components/School.vue';
+import Student from '@/components/Student.vue';
+
+// 1. 有多个相同的 ref 时，取最后一个
+// 2. ref 作用于原生标签，获得到 dom 元素，作用于组件标签，获取到组件对象
+// 3. 使用 id，getDocumentById 获取组件元素，得到内部的 dom 对象
+export default {
+  name: 'App',
+  components: {
+    Student,
+    School,
+  },
+  methods: {
+    show() {
+      console.log(this.$refs.btn);
+    },
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ AGG/Vue2021/mine/02_vue_test/src_02_ref属性/components/Student.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h4>姓名：{{ name }}</h4>
+    <h4>年龄：{{ age }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: 'John',
+      age: 11,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
